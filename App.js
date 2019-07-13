5815d99d47f29a2ca9c020bed577bf4e7658097a
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2019-07-12 22:55:01 -0300

    fixing user notification

--- App.js
+++ App.js
@@ -3,17 +3,18 @@ import { Provider } from 'react-redux'
 import Navigator from './src/navigations/navigator'
 import { createStore } from 'redux'
 import reducers from './src/redux/reducers'
-
-import { clearLocalNotification, setLocalNotification } from './src/utils/notification'
-
-const App = () => {
-    clearLocalNotification()
-    setLocalNotification()
-    return (
-      <Provider store={createStore(reducers)}>
-          <Navigator />
-      </Provider>
-     )
+import { setLocalNotification } from './src/utils/notification'
+class App extends Component {
+    componentDidMount() {
+        setLocalNotification()
+    }
+    render(){
+        return (
+            <Provider store={createStore(reducers)}>
+                <Navigator />
+            </Provider>
+        )
+    }
 }
 
 export default App
